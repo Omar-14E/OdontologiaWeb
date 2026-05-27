dcfa4ca3b09392d284685ff5c18113fefc3d0a59
its-Noemi <noeliacusquisiban@gmail.com>
2026-05-27 15:06:31 -0500

    Ahora se puede mostrar la lista de medicos en la clinica sin necesida de dar doble clic si entras como admin

--- frontend/src/app/components/citas/lista-citas/lista-citas.ts
+++ frontend/src/app/components/citas/lista-citas/lista-citas.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID, ChangeDetectorRef } from '@angular/core'; // <-- Importamos las herramientas
+import { CommonModule, isPlatformBrowser } from '@angular/common'; // <-- isPlatformBrowser
 import { Router, RouterModule } from '@angular/router';
 import { CitaService } from '../../../services/cita';
 import { Cita } from '../../../models/cita';
@@ -8,21 +8,38 @@ import { Cita } from '../../../models/cita';
   selector: 'app-lista-citas',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  templateUrl: './lista-citas.html', // Sin .component
-  styleUrl: './lista-citas.css'      // Sin .component
+  templateUrl: './lista-citas.html',
+  styleUrl: './lista-citas.css'
 })
 export class ListaCitasComponent implements OnInit {
-  // ... resto del código igual que el que te pasé ...
   citas: Cita[] = [];
 
-  constructor(private citaService: CitaService, private router: Router) {}
+  constructor(
+    private citaService: CitaService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object, // <-- Inyectamos plataforma
+    private cdr: ChangeDetectorRef                   // <-- Inyectamos el detector
+  ) {}
 
   ngOnInit(): void {
-    this.cargarCitas();
+    // 1. Bloqueamos al servidor (SSR) para que no haga la petición sin token
+    if (isPlatformBrowser(this.platformId)) {
+      this.cargarCitas();
+    }
   }
 
   cargarCitas() {
-    this.citaService.getCitas().subscribe(datos => this.citas = datos);
+    this.citaService.getCitas().subscribe({
+      next: (datos) => {
+        this.citas = datos;
+
+        // 2. Obligamos a Angular a dibujar la tabla instantáneamente
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Error al cargar las citas:', err);
+      }
+    });
   }
 
   eliminar(id: number) {
