5c5e875a9b1fb71d074d9a5cb917b7b37dcc5dce
its-Noemi <noeliacusquisiban@gmail.com>
2026-07-01 16:10:49 -0500

    Se creo la ruta del componente Mi horario y se implmento en odontologo

--- frontend-clinica/src/app/app.routes.ts
+++ frontend-clinica/src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { AdminMedicosComponent } from './features/admin-medicos/admin-medicos';
 import { AdminPacientesComponent } from './features/admin-pacientes/admin-pacientes';
 import { AdminTurnosComponent } from './features/admin-turnos/admin-turnos';
 import { AdminHistorialCitasComponent } from './features/admin-historial-citas/admin-historial-citas';
+import { OdontoHorarioComponent } from './features/odonto-horario/odonto-horario';
 
 export const routes: Routes = [
   
@@ -57,6 +58,13 @@ export const routes: Routes = [
     canActivate: [authGuard]
 
   },
+   {
+    path: 'odonHorario',
+    component: OdontoHorarioComponent,
+    canActivate: [authGuard]
+
+  },
+  
   
   // --- Nuevas rutas del Administrador (Protegidas) ---
   {
